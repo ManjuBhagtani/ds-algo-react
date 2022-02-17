a8728c826594f7eb822710b23fc90415f5ef989f
manju <manjubhagtani@gmail.com>
2022-02-17 19:31:16 +0530

    Added ProblemContainer to PrefixSum component

--- src/components/Arrays/PrefixSum/PrefixSum.js
+++ src/components/Arrays/PrefixSum/PrefixSum.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import UIConstants from '../../Constants/UIConstants';
 import CommonInfoContainer from '../../Common/CommonInfoContainer';
+import ProblemContainer from '../../Common/ProblemContainer';
 
 function PrefixSum(){
         return(
-            <CommonInfoContainer
-                topicName={UIConstants.topicNames.PREFIXSUM}
-                topicDescription={UIConstants.topicDescriptions.PREFIXSUM}
-            />
+            <div className="p-3">
+                <CommonInfoContainer
+                    topicName={UIConstants.topicNames.PREFIXSUM}
+                    topicDescription={UIConstants.topicDescriptions.PREFIXSUM}
+                />
+                <ProblemContainer
+                    problems={UIConstants.problems.PREFIXSUM}
+                />
+            </div>
         );
 }
 export default PrefixSum;
