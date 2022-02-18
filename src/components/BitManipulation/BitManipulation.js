68578fe7f19b3fc4390b10e900e7d5832fb6c608
manju <manjubhagtani@gmail.com>
2022-02-18 19:43:59 +0530

    Added desc, tricks, problems for BitManipulation

--- src/components/BitManipulation/BitManipulation.js
+++ src/components/BitManipulation/BitManipulation.js
@@ -1,11 +1,26 @@
-import React, { Component } from 'react';
-class BitManipulation extends Component{
-    render(){
+import React from 'react';
+import UIConstants from '../Constants/UIConstants';
+import CommonInfoContainer from '../Common/CommonInfoContainer';
+import ProblemContainer from '../Common/ProblemContainer';
+import NotesFile from '../../assets/Bits_1.pdf';
+import Tricks from '../Common/Tricks';
+
+function BitManipulation(){
         return(
-            <div>
-                <h1>Bit Manipulation</h1>
+            <div className="p-3">
+                <CommonInfoContainer
+                    topicName={UIConstants.topicNames.BITMANIPULATION}
+                    topicDescription={UIConstants.topicDescriptions.BITMANIPULATION}
+                    notesFileName={UIConstants.notesFileNames.BITMANIPULATION}
+                    notesFile={NotesFile}
+                />
+                <Tricks
+                    tricks={UIConstants.tricks.BITMANIPULATION}
+                />
+                <ProblemContainer
+                    problems={UIConstants.problems.BITMANIPULATION}
+                />
             </div>
         );
-    }
 }
 export default BitManipulation;
