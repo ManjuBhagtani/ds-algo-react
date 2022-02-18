843eccc29cc014d7a902b5cefecf5e8700962e6c
manju <manjubhagtani@gmail.com>
2022-02-18 23:41:16 +0530

    Added Common container to Subarrays component

--- src/components/Arrays/Subarrays/Subarrays.js
+++ src/components/Arrays/Subarrays/Subarrays.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import UIConstants from '../../Constants/UIConstants';
 import ProblemContainer from '../../Common/ProblemContainer';
+import CommonInfoContainer from '../../Common/CommonInfoContainer';
+import NotesFile from '../../../assets/Arrays_Subarrays.pdf';
 
 function Subarrays(){
         return(
             <div className="p-3">
+                <CommonInfoContainer
+                    topicName={UIConstants.topicNames.SUBARRAYS}
+                    //topicDescription={UIConstants.topicDescriptions.SUBARRAYS}
+                    notesFileName={UIConstants.notesFileNames.SUBARRAYS}
+                    notesFile={NotesFile}
+                />
                 <ProblemContainer
                     problems={UIConstants.problems.SUBARRAYS}
                 />
