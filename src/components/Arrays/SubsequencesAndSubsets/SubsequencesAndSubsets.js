ec755546762c472921cb3be52ab5a764c804f43e
manju <manjubhagtani@gmail.com>
2022-02-18 23:41:38 +0530

    Added Common container to Subsequences component

--- src/components/Arrays/SubsequencesAndSubsets/SubsequencesAndSubsets.js
+++ src/components/Arrays/SubsequencesAndSubsets/SubsequencesAndSubsets.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import UIConstants from '../../Constants/UIConstants';
 import ProblemContainer from '../../Common/ProblemContainer';
+import CommonInfoContainer from '../../Common/CommonInfoContainer';
+import NotesFile from '../../../assets/Subsequences_and_Subset.pdf';
 
 function SubsequencesAndSubsets(){
         return(
             <div className="p-3">
+                <CommonInfoContainer
+                    topicName={UIConstants.topicNames.SUBSEQUENCESANDSUBSETS}
+                    //topicDescription={UIConstants.topicDescriptions.SUBARRAYS}
+                    notesFileName={UIConstants.notesFileNames.SUBSEQUENCESANDSUBSETS}
+                    notesFile={NotesFile}
+                />
                 <ProblemContainer
                     problems={UIConstants.problems.SUBSEQUENCESANDSUBSETS}
                 />
