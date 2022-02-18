720b8c34d6c6c710bc81a0363608dcadd52c87ae
manju <manjubhagtani@gmail.com>
2022-02-18 23:42:10 +0530

    Added Common container to Strings component

--- src/components/Strings/Strings.js
+++ src/components/Strings/Strings.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import UIConstants from '../Constants/UIConstants';
 import ProblemContainer from '../Common/ProblemContainer';
+import CommonInfoContainer from '../Common/CommonInfoContainer';
+import NotesFile from '../../assets/Strings_basics.pdf';
 
 function Strings(){
         return(
             <div className="p-3">
+                <CommonInfoContainer
+                    topicName={UIConstants.topicNames.STRINGS}
+                    //topicDescription={UIConstants.topicDescriptions.STRINGS}
+                    notesFileName={UIConstants.notesFileNames.STRINGS}
+                    notesFile={NotesFile}
+                />
                 <ProblemContainer
                     problems={UIConstants.problems.STRINGS}
                 />
