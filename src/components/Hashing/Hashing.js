974fc78721238caeb9751b521ae2256b1c6a4b72
manju <manjubhagtani@gmail.com>
2022-02-18 23:41:56 +0530

    Added Common container to Hashing component

--- src/components/Hashing/Hashing.js
+++ src/components/Hashing/Hashing.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import UIConstants from '../Constants/UIConstants';
 import ProblemContainer from '../Common/ProblemContainer';
+import CommonInfoContainer from '../Common/CommonInfoContainer';
+import NotesFile from '../../assets/Hashing_1.pdf';
 
 function Hashing(){
         return(
             <div className="p-3">
+                <CommonInfoContainer
+                    topicName={UIConstants.topicNames.HASHING}
+                    //topicDescription={UIConstants.topicDescriptions.HASHING}
+                    notesFileName={UIConstants.notesFileNames.HASHING}
+                    notesFile={NotesFile}
+                />
                 <ProblemContainer
                     problems={UIConstants.problems.HASHING}
                 />
