cc8c5647f01ee16cfd5179c766ca8b3b7457c788
manju <manjubhagtani@gmail.com>
2022-02-18 23:40:46 +0530

    Added CommonContainer to CarryForward

--- src/components/Arrays/CarryForward/CarryForward.js
+++ src/components/Arrays/CarryForward/CarryForward.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import UIConstants from '../../Constants/UIConstants';
 import ProblemContainer from '../../Common/ProblemContainer';
+import CommonInfoContainer from '../../Common/CommonInfoContainer';
+import NotesFile from '../../../assets/Strings_basics.pdf';
 
 function CarryForward(){
         return(
             <div className="p-3">
+                <CommonInfoContainer
+                    topicName={UIConstants.topicNames.CARRYFORWARD}
+                    //topicDescription={UIConstants.topicDescriptions.CARRYFORWARD}
+                    notesFileName={UIConstants.notesFileNames.CARRYFORWARD}
+                    notesFile={NotesFile}
+                />
                 <ProblemContainer
                     problems={UIConstants.problems.CARRYFORWARD}
                 />
